4121c0c256a6e75c581225fbbf6955d5735011ec
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 11:57:18 -0300

    feat: add class to customer

---
+++ src/lib/user/Customer.ts
@@ -0,0 +1,18 @@
+import User from "./User";
+
+class Customer extends User {
+  public constructor(
+    protected _id: string,
+    protected _name: string,
+    protected _email: string,
+    protected _password: string,
+    protected _createdAt: Date,
+    protected _deletedAt?: Date,
+    protected _lastname?: string,
+    protected _image?: string,
+  ) {
+    super(_id, _name, _email, _password, _createdAt, _deletedAt, _lastname, _image);
+  }
+}
+
+export default Customer;
