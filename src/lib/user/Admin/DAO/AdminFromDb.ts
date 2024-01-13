1dbc8bf3259b7c509e6f8659f34f1eed2727c733
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 13:39:15 -0300

    feat: add type to adminfromdb

---
+++ src/lib/user/Admin/DAO/AdminFromDb.ts
@@ -0,0 +1,13 @@
+type AdminFromDb = {
+  id: string,
+  client_id: string,
+  name: string,
+  last_name: string,
+  email: string,
+  image: string,
+  password: string,
+  created_at: string,
+  deleted_at: string,
+}
+
+export default AdminFromDb
