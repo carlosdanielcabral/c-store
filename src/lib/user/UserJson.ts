8db2a89ce24d34ee8840371861db5b1b1cd1ff85
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 13:37:04 -0300

    feat: add type userjson

---
+++ src/lib/user/UserJson.ts
@@ -0,0 +1,11 @@
+type UserJson = {
+  id: string,
+  name: string,
+  email: string,
+  createdAt: string,
+  deletedAt: string,
+  image?: string,
+  lastName?: string,
+}
+
+export default UserJson;
