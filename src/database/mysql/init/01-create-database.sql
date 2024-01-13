32ba310401fba084a81de33c3657eaafd0e8ec4d
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 12:48:21 -0300

    fix: errors when creatin database

--- src/database/mysql/init/01-create-database.sql
+++ src/database/mysql/init/01-create-database.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `c-store`.`category` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
-
 -- -----------------------------------------------------
 -- Table `c-store`.`client`
 -- -----------------------------------------------------
@@ -31,6 +30,18 @@ CREATE TABLE IF NOT EXISTS `c-store`.`client` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
+-- -----------------------------------------------------
+-- Table `c-store`.`payment_method`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `c-store`.`payment_method` ;
+
+CREATE TABLE IF NOT EXISTS `c-store`.`payment_method` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `thumbnail` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `c-store`.`client_payment_method`
@@ -54,21 +65,105 @@ CREATE TABLE IF NOT EXISTS `c-store`.`client_payment_method` (
     REFERENCES `c-store`.`payment_method` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
--- Table `c-store`.`payment_method`
+-- Table `c-store`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`payment_method` ;
+DROP TABLE IF EXISTS `c-store`.`user` ;
 
-CREATE TABLE IF NOT EXISTS `c-store`.`payment_method` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `thumbnail` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `c-store`.`user` (
+  `id` CHAR(36) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(150) NOT NULL,
+  `image` VARCHAR(255) NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `deleted_at` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
+-- -----------------------------------------------------
+-- Table `c-store`.`user_admin`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `c-store`.`user_admin` ;
+
+CREATE TABLE IF NOT EXISTS `c-store`.`user_admin` (
+  `id` CHAR(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_admin_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `c-store`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+-- -----------------------------------------------------
+-- Table `c-store`.`user_customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `c-store`.`user_customer` ;
+
+CREATE TABLE IF NOT EXISTS `c-store`.`user_customer` (
+  `id` CHAR(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_customer_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `c-store`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+-- -----------------------------------------------------
+-- Table `c-store`.`user_customer_address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `c-store`.`user_customer_address` ;
+
+CREATE TABLE IF NOT EXISTS `c-store`.`user_customer_address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` CHAR(36) NOT NULL,
+  `address` VARCHAR(150) NOT NULL,
+  `city` VARCHAR(150) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `cep` CHAR(8) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `deleted_at` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_customer_address_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `c-store`.`user_customer` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+-- -----------------------------------------------------
+-- Table `c-store`.`user_saler`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `c-store`.`user_saler` ;
+
+CREATE TABLE IF NOT EXISTS `c-store`.`user_saler` (
+  `id` CHAR(36) NOT NULL,
+  `client_id` CHAR(36) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_saler_client_id_idx` (`client_id` ASC) VISIBLE,
+  CONSTRAINT `fk_user_saler_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `c-store`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_saler_client_id`
+    FOREIGN KEY (`client_id`)
+    REFERENCES `c-store`.`client` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `c-store`.`product`
@@ -93,8 +188,8 @@ CREATE TABLE IF NOT EXISTS `c-store`.`product` (
     REFERENCES `c-store`.`client` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `c-store`.`product_category`
@@ -116,8 +211,8 @@ CREATE TABLE IF NOT EXISTS `c-store`.`product_category` (
     REFERENCES `c-store`.`product` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `c-store`.`product_image`
@@ -137,29 +232,21 @@ CREATE TABLE IF NOT EXISTS `c-store`.`product_image` (
     REFERENCES `c-store`.`product` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
--- Table `c-store`.`product_rating`
+-- Table `c-store`.`sale_status`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`product_rating` ;
+DROP TABLE IF EXISTS `c-store`.`sale_status` ;
 
-CREATE TABLE IF NOT EXISTS `c-store`.`product_rating` (
+CREATE TABLE IF NOT EXISTS `c-store`.`sale_status` (
   `id` INT NOT NULL,
-  `sale_id` CHAR(36) NULL,
-  `rating` TINYINT(1) NOT NULL,
-  `comment` VARCHAR(45) NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`),
-  INDEX `fk_product_rating_sale_id_idx` (`sale_id` ASC) VISIBLE,
-  CONSTRAINT `fk_product_rating_sale_id`
-    FOREIGN KEY (`sale_id`)
-    REFERENCES `c-store`.`sale` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
 -- Table `c-store`.`sale`
@@ -201,117 +288,26 @@ CREATE TABLE IF NOT EXISTS `c-store`.`sale` (
     REFERENCES `c-store`.`sale_status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
 -- -----------------------------------------------------
--- Table `c-store`.`sale_status`
+-- Table `c-store`.`product_rating`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`sale_status` ;
+DROP TABLE IF EXISTS `c-store`.`product_rating` ;
 
-CREATE TABLE IF NOT EXISTS `c-store`.`sale_status` (
+CREATE TABLE IF NOT EXISTS `c-store`.`product_rating` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `c-store`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `c-store`.`user` (
-  `id` CHAR(36) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NULL,
-  `email` VARCHAR(150) NOT NULL,
-  `image` VARCHAR(255) NULL,
-  `password` VARCHAR(255) NOT NULL,
+  `sale_id` CHAR(36) NULL,
+  `rating` TINYINT(1) NOT NULL,
+  `comment` VARCHAR(45) NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `deleted_at` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `c-store`.`user_admin`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`user_admin` ;
-
-CREATE TABLE IF NOT EXISTS `c-store`.`user_admin` (
-  `id` CHAR(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_user_admin_id`
-    FOREIGN KEY (`id`)
-    REFERENCES `c-store`.`user` (`id`)
+  INDEX `fk_product_rating_sale_id_idx` (`sale_id` ASC) VISIBLE,
+  CONSTRAINT `fk_product_rating_sale_id`
+    FOREIGN KEY (`sale_id`)
+    REFERENCES `c-store`.`sale` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `c-store`.`user_customer`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`user_customer` ;
-
-CREATE TABLE IF NOT EXISTS `c-store`.`user_customer` (
-  `id` CHAR(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_user_customer_id`
-    FOREIGN KEY (`id`)
-    REFERENCES `c-store`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `c-store`.`user_customer_address`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`user_customer_address` ;
-
-CREATE TABLE IF NOT EXISTS `c-store`.`user_customer_address` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` CHAR(36) NOT NULL,
-  `address` VARCHAR(150) NOT NULL,
-  `city` VARCHAR(150) NOT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  `cep` CHAR(8) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `deleted_at` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_user_customer_address_user_id`
-    FOREIGN KEY ()
-    REFERENCES `c-store`.`user_customer` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `c-store`.`user_saler`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `c-store`.`user_saler` ;
-
-CREATE TABLE IF NOT EXISTS `c-store`.`user_saler` (
-  `id` CHAR(36) NOT NULL,
-  `client_id` CHAR(36) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_user_saler_client_id_idx` (`client_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_saler_id`
-    FOREIGN KEY (`id`)
-    REFERENCES `c-store`.`user` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_user_saler_client_id`
-    FOREIGN KEY (`client_id`)
-    REFERENCES `c-store`.`client` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
