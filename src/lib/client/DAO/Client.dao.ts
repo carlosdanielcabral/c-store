c8900b8cd70259ff2fa8a38c2c6196b26f11aaf2
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 11:45:17 -0300

    feat: add dao to client

---
+++ src/lib/client/DAO/Client.dao.ts
@@ -0,0 +1,17 @@
+import Connection from "../../../database/Connection";
+import CreateClientDTO from "../DTO/CreateClient.dto";
+
+class ClientDAO {
+  public constructor(
+    private _connection: Connection = new Connection(),
+  ) {}
+
+  public create(client: CreateClientDTO): void {
+    const query = "INSERT INTO client (id, name, cnpj, created_at) VALUES (?, ?, ?, NOW())";
+    const values = [client.id, client.name, client.cnpj];
+
+    this._connection.execute(query, values);
+  }
+}
+
+export default ClientDAO;
