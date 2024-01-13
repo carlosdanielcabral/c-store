4fabf57d2a4276a81958a79cf110a4b94f32d1c7
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 12:05:15 -0300

    feat: add dto to create user

---
+++ src/lib/user/DTO/CreateUser.dto.ts
@@ -0,0 +1,14 @@
+import { randomUUID } from "crypto";
+
+class CreateUserDTO {
+  public constructor(
+    public id: string = randomUUID(),
+    public name: string,
+    public email: string,
+    public password: string,
+    public lastname: string = '',
+    public image: string = '',
+  ) {}
+}
+
+export default CreateUserDTO;
