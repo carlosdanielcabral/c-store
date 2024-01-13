afac6eb05c6eec780d9c8d349aae0f5859314e7e
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-13 12:05:34 -0300

    feat: add dao to user

---
+++ src/lib/user/DAO/User.dao.ts
@@ -0,0 +1,17 @@
+import Connection from "../../../database/Connection";
+import CreateUserDTO from "../DTO/CreateUser.dto";
+
+class UserDAO {
+  public constructor(
+    private _connection: Connection = new Connection(),
+  ) {}
+
+  public create(user: CreateUserDTO): void {
+    const query = "INSERT INTO client (id, name, last_name, email, password, image) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+    const values = [user.id, user.name, user.lastname, user.email, user.password, user.image];
+
+    this._connection.execute(query, values);
+  }
+}
+
+export default UserDAO;
