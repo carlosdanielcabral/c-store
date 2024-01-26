746edd656dcd55bcd011c40edafdef3072287336
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-26 20:37:40 -0300

    feat: add repository to payment method

---
+++ src/lib/payment-method/Repository/PaymentMethod.repository.ts
@@ -0,0 +1,21 @@
+import PaymentMethodDAO from '../DAO/PaymentMethod.dao';
+import PaymentMethod from '../PaymentMethod';
+
+class PaymentMethodRepository {
+  public constructor(protected _dao: PaymentMethodDAO = new PaymentMethodDAO()) {
+  }
+
+  public getById = async (id: number): Promise<PaymentMethod> => {
+    const fetch = await this._dao.getById(id);
+
+    return PaymentMethod.fromFetch(fetch);
+  };
+
+  public getAllByClient = async (clientId: string): Promise<PaymentMethod[]> => {
+    const fetch = await this._dao.getAllByClient(clientId);
+
+    return fetch.map(data => PaymentMethod.fromFetch(data));
+  }
+}
+
+export default PaymentMethodRepository;
