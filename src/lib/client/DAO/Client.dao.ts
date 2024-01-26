2966d766fe3ff94d8ce70d7f037e23a0d41c4b90
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-26 20:13:50 -0300

    feat(DAO): add method to get client by id

--- src/lib/client/DAO/Client.dao.ts
+++ src/lib/client/DAO/Client.dao.ts
@@ -1,16 +1,26 @@
 import Connection from '../../../database/Connection';
 import CreateClientDTO from '../DTO/CreateClient.dto';
+import { Fetch } from '../types';
 
 class ClientDAO {
   public constructor(private _connection: Connection = new Connection()) {}
 
-  public create(client: CreateClientDTO): void {
+  public create = async (client: CreateClientDTO): Promise<void> => {
     const query =
       'INSERT INTO client (id, name, cnpj, created_at) VALUES (?, ?, ?, NOW())';
     const values = [client.id, client.name, client.cnpj];
 
-    this._connection.execute(query, values);
-  }
+    await this._connection.execute(query, values);
+  };
+
+  public getById = async (id: string): Promise<Fetch> => {
+    const query = 'SELECT * FROM client WHERE id = ?';
+    const values = [id];
+
+    const [result] = await this._connection.execute(query, values);
+
+    return result as unknown as Fetch;
+  };
 }
 
 export default ClientDAO;
