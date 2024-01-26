532054be23f7115f643dca232c29b5f1f4627ad2
Carlos Cabral <dev.carlosdaniel@gmail.com>
2024-01-26 20:37:19 -0300

    feat: add dao to payment method

---
+++ src/lib/payment-method/DAO/PaymentMethod.dao.ts
@@ -0,0 +1,34 @@
+import Connection from '../../../database/Connection';
+import { Fetch } from '../types';
+
+class PaymentMethodDAO {
+  public constructor(protected _connection: Connection = new Connection()) {
+  }
+
+  public getById = async (id: number): Promise<Fetch> => {
+    const query =
+      'SELECT * FROM payment_method WHERE id = ?';
+    const values = [id];
+
+    const [result] = await this._connection.execute(query, values);
+
+    return result as unknown as Fetch;
+  };
+
+  public getAllByClient = async (clientId: string): Promise<Fetch[]> => {
+    const query =  `
+      SELECT pm.*
+      FROM payment_method pm
+      INNER JOIN client_payment_method cpm
+        ON cpm.payment_method_id = pm.id
+      WHERE cpm.client_id = ?
+    `;
+    const values = [clientId];
+
+    const [result] = await this._connection.execute(query, values);
+
+    return result as unknown as Fetch[];
+  };
+}
+
+export default PaymentMethodDAO;
